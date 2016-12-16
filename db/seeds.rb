84acda4b6c9b50501ef9601d1a60ca05f96a26d8
Glenn Basgaard <glenn_basgaard@yahoo.com>
2016-12-16 09:32:48 -0500

    testing git merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Rum.delete_all
+
+Rum.create({name: "Captain Morgan", country: "USA", price: 12}, {name: "Bacardi", country: "Cuba", price: 10}, {name: "Rummy Rum", country: "Swaziland", price: 22})
