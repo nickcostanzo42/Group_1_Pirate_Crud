bad6c196b7a4d91d5930bfb0dd5e21b18deff28d
Glenn Basgaard <glenn_basgaard@yahoo.com>
2016-12-16 10:01:25 -0500

    add controller methods some more

--- app/controllers/rums_controller.rb
+++ app/controllers/rums_controller.rb
@@ -1,6 +1,10 @@
 class RumsController < ApplicationController
   def create
-
+    rum = params['rum']
+    Rum.create(name: rum['name'],
+               country: rum['country'],
+               price: rum['price'])
+    redirect_to(:back)
   end
 
   def update
